e938b62f4ca212afbeff18cd97de31c259eab0bc
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-12-08 10:07:12 -0600

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import {Router, Link, Route} from "svelte-routing";
+	import Home from './views/Home.svelte'
+	import Login from './views/Login.svelte'
+	import Agenda from './views/Agenda.svelte'
+	import Empleados from './views/Empleados.svelte'
+	import EditarEmpleados from './views/EditarEmpleados.svelte'
+	import Navbar from './components/Navbar.svelte'
+	import {onMount} from 'svelte'
+	import {user} from './stores/User'
+	
+
+	onMount(async() =>{
+		await user.current()
+	})
+	
+    
+</script>
+
+{#if $user === false}
+	<h1>Cargando ...</h1>
+{:else}
+	<div>
+		
+		<Router>
+			<Route path="/">
+				<Home />		
+			</Route>
+			
+			<Route path="/login">
+				<Login></Login>
+			</Route>
+			<Route path="/agenda">
+				<Agenda />
+			</Route>
+			
+			<Route path="/empleados">
+				<Empleados/>
+			</Route>
+			<Route path="/empleados/ver">
+				<EditarEmpleados />
+			</Route>
+			
+		</Router>
+	</div>
+{/if}
+
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+
+</style>
---
+++ src/components/Boton.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Link } from "svelte-routing";
+    export let tituloBoton;
+    export let urlBoton;
+</script>
+
+<div>
+    <Link to="{urlBoton}" class="block px-4 py-2 text-sm text-red-600 hover:text-gray-700 border-red-600 hover:border-gray-700" role="menuitem">{tituloBoton}</Link> 
+</div>
