a375eb7370fbaf48514fb2e710bd38f01c7defe7
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-12-21 12:12:19 -0600

    Update Empleados.svelte

--- src/views/Empleados.svelte
+++ src/views/Empleados.svelte
@@ -15,7 +15,7 @@
    
   
     let data = [];
-    const API = "apollo:3001/empleados";
+    const API = "backend:3001/empleados";
     onMount(async () => {
       const response = await fetch(API);
       data = await response.json();
@@ -36,7 +36,7 @@
     };
   
     const getUser = async (id) => {
-      const APII = `apollo:3001/empleados/${id}`;
+      const APII = `backend:3001/empleados/${id}`;
       const response = await fetch(APII);
       row = await response.json();
       open = true
